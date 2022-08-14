e1e34e6199260765879dfb8f0b8b2d477fdf2df8
YHJ96 <yhj878812@naver.com>
2022-08-14 18:30:25 +0900

    feat: 이미지 파일 모듈 작성 완료

--- src/images/index.js
+++ src/images/index.js
@@ -4,6 +4,8 @@ import plusImg from './plus.png';
 import compassImg from './compass.png';
 import loveImg from './love.png';
 import avatarImg from './avatar.png';
+import moreImg from './more.png';
+import textImg from './text.png';
 
 export const home = homeImg;
 export const arrow = arrowImg;
@@ -11,3 +13,5 @@ export const plus = plusImg;
 export const compass = compassImg;
 export const love = loveImg;
 export const avatar = avatarImg;
+export const more = moreImg;
+export const text = textImg;
