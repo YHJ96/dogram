ab46ea1261344ad927a0f940aef03cf714288809
YHJ96 <yhj878812@naver.com>
2022-08-14 16:22:27 +0900

    feat: ResetStyle 설정 완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from './App';
+import { ResetStyle } from "./ResetStyle";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<App/>);
+root.render(
+  <React.Fragment>
+    <ResetStyle />
+    <App />
+  </React.Fragment>
+);
