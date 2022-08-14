d531ad7c4abb6f73436fd821d9938681675afcb6
YHJ96 <yhj878812@naver.com>
2022-08-14 16:23:27 +0900

    feat: 이미지 파일 모듈 작성 완료

---
+++ src/images/index.js
@@ -0,0 +1,13 @@
+import homeImg from './home.png';
+import arrowImg from './arrow.png';
+import plusImg from './plus.png';
+import compassImg from './compass.png';
+import loveImg from './love.png';
+import avatarImg from './avatar.png';
+
+export const home = homeImg;
+export const arrow = arrowImg;
+export const plus = plusImg;
+export const compass = compassImg;
+export const love = loveImg;
+export const avatar = avatarImg;
