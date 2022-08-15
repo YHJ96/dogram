b1ce4493c5131ff5c8d25f918e1c2f17e2bba32d
YHJ96 <yhj878812@naver.com>
2022-08-15 21:58:47 +0900

    feat: 피드 생성 구현 완료

--- src/page/App.jsx
+++ src/page/App.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "../components/NavBar";
 import Feed from "../components/Feed";
 import Recommend from "../components/Recommend";
 import { MainContainer, FeedSection } from '../page/style';
 
 function App() {
+    const [feedData, setFeedData] = useState([]);
+
+    const createFeed = () => {
+        return feedData.map((item, index) => {
+            return <Feed key={index} FeedImgSrc={item} />
+        });
+    };
+
     return (
         <React.Fragment>
-            <NavBar />
+            <NavBar feedData={feedData} setFeedData={setFeedData} />
             <MainContainer>
                 <FeedSection>
-                    <Feed />
-                    <Feed />
+                    {createFeed()}
                 </FeedSection>
                 <Recommend />
             </MainContainer>
