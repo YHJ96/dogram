0bde3d9d8bd0adea2637bbc638463a2d3d7e6244
YHJ96 <yhj878812@naver.com>
2022-08-17 12:56:48 +0900

    refactor: console 함수 제거 완료

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,7 +1,6 @@
 export const debounce = (callback, delay) => {
   let timer;
   return (...args) => {
-    console.log(args);
     clearTimeout(timer);
     timer = setTimeout(() => callback(...args), delay);
   }
